04a6644dbd04e95429c937fed817b80963fed578
martins1015 <evertonjunior1015@hotmail.com>
2023-10-23 13:48:43 -0300

    up files

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Devido',
+        status: 'Pendente'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Reembolso',
+        status: 'Recusado'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+]
